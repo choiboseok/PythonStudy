69a399dbfedcebf3d14f9936e8bcbdb722144dc1
choiboseok <chlqhtjr30@gmail.com>
2025-04-03 17:51:08 +0900

    프로젝트 크롤링

---
+++ Project/라라아쿠아/라라아쿠아어항_60.py
@@ -0,0 +1,54 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from openpyxl import Workbook
+import time
+import os
+img_path = '../testimg'
+if not os.path.exists(img_path):
+    os.mkdir(img_path)
+
+wb = Workbook()
+ws = wb.active
+
+
+url = 'https://raraaqua.com/category/60cm-90cm%EB%AF%B8%EB%A7%8C/622/'
+driver = webdriver.Chrome()
+driver.implicitly_wait(3)
+driver.get(url)
+time.sleep(1)
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+driver.quit()
+img_list = []
+text_list = []
+price_list = []
+div = soup.select_one("ul.prdList")
+lis = div.find_all('li')
+for li in lis:
+    img = li.select_one("img")
+    if img:
+        # 이미지, 제목 가져오기
+        src = img.get('src')
+        alt = img.get('alt')
+        img_list.append(src)
+        text_list.append(alt)
+        print("https:"+src)
+        print(alt)
+    # 가격 가져오기
+    description = li.select_one("div.description")
+    if description:
+        ul = description.select_one("ul")
+        spans = ul.find_all('span')
+        price = spans[1].text
+        price_list.append(price)
+        print(spans[1].text)
+
+n=1
+for t, i, p in zip(text_list, img_list, price_list) :
+    ws[f'A{n}'] = t
+    ws[f'B{n}'] = i
+    ws[f'C{n}'] = p
+    ws[f'D{n}'] = '어항'
+    n+=1
+
+wb.save("어항데이터(라라아쿠아).xlsx")
+wb.close()
---
+++ Project/세진아쿠아/엑셀toDB_세진아쿠아.py
@@ -0,0 +1,66 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from openpyxl import Workbook
+# openpyxl라이브러리에서 load_workbook을 import
+from openpyxl import load_workbook
+from operator import itemgetter
+import urllib.request as req
+import requests
+import os
+import requests
+import json
+import cx_Oracle
+
+class DBManager:
+    def __init__(self):
+        self.conn = None
+
+    def get_connection(self):
+        try:
+            if self.conn is None or self.conn.closed:
+                self.conn = cx_Oracle.connect("Pro","pro","localhost:1521/xe")
+            return self.conn
+        except Exception as e:
+            return None
+    def __del__(self):
+        """객체 소멸 시 연결 종료"""
+        if self.conn:
+            self.conn.close()
+            print("db 연결이 정상적으로 종료되었습니다.")
+
+    def insert(self, query, param):
+        """데이터 삽입"""
+        cursor = None
+        try:
+            if self.conn is None:
+                self.get_connection()
+            cursor = self.conn.cursor()
+            cursor.execute(query, param)
+            self.conn.commit()
+        except Exception as e:
+            if self.conn:
+                self.conn.rollback()
+        finally:
+            if cursor:
+                cursor.close()
+
+
+if __name__ == '__main__':
+    sql = """
+            INSERT
+            INTO fish_tank(title, img, price)
+            VALUES(:1, :2, :3)
+        """
+    # '.xlsx' 엑셀 파일 불러오기
+    wb = load_workbook(filename='어항데이터_세진아쿠아.xlsx')
+    ws = wb['어항']
+    n = 1
+
+    db = DBManager()
+    conn = db.get_connection()
+    while True:
+        if ws[f'A{n}'].value == None:
+            break
+        if conn:
+            db.insert(sql, [ws[f'A{n}'].value, ws[f'B{n}'].value, ws[f'C{n}'].value])
+        n+=1
